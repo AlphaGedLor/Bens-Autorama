b56e9d1a5b815cb736c2c33aa8c8c199f38a7bf4
AlphaGedLor <lorne.geddes.cet@hotmail.com>
2016-05-13 19:52:47 -0600

    see this

--- _site/assets/js/sidebar.js
+++
@@ -1,8 +0,0 @@
-$('.site-links-mobile').click(function(){
-  var $inputElement = $('#sidebartoggler').prop('checked');
-  console.log($inputElement);
-  if ($inputElement) {
-    $('#sidebartoggler').prop('checked', false).delay(6000);
-    console.log($inputElement);
-  }
-});
